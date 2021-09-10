22004ec9542c4bc8340c05447b335b2990b52109
kidotedla <tedlakidus0@gmail.com>
2021-09-10 09:32:04 -0700

    p on n

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Function that tells a random nos positivity
+ * Return: 0 since void
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
