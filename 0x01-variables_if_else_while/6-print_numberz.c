61cc88b9781f5fcbd879af08426f4ec6f66b4ec3
kidotedla <tedlakidus0@gmail.com>
2021-09-11 06:27:48 -0700

    print numbers2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - numbers
+ * Return: numbers
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
