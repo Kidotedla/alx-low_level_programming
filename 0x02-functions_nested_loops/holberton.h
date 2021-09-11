cd356f690e5e2d5be3575a59353f102fb7eb0d23
kidotedla <tedlakidus0@gmail.com>
2021-09-11 07:07:39 -0700

    holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,15 +1,21 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _putchar(char c);
-char *_strcat(char* dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-void print_number(int n);
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
+int add (int, int);
+void print_times_table(int n);
 
 #endif
