2ab42ea0f30e83eb27ea437535a1c4cf4ef52e57
kidotedla <tedlakidus0@gmail.com>
2021-09-11 06:15:26 -0700

    variable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - alpahabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
