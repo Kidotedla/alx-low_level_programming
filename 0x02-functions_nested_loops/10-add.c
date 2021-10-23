023ea7089254308038ea7c72d520dc7bd5e54068
kidotedla <tedlakidus0@gmail.com>
2021-10-23 10:49:23 -0700

    0x02 fun

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers a and b.
+ * @a: integer to be added.
+ * @b: second integer.
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
