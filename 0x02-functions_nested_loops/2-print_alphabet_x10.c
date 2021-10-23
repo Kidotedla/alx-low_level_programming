461842a527b1d289efe3574761bab1231910c829
kidotedla <tedlakidus0@gmail.com>
2021-10-23 10:18:00 -0700

    0x02 fun

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times lower case
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a, b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 96; a < 122; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
