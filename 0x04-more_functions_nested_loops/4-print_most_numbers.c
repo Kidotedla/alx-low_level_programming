87ac3bd107bb0e3b71761901af75c0b94901bd9f
kidotedla <tedlakidus0@gmail.com>
2021-10-25 11:05:13 -0700

    0x04 mor fun

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - zero to ten.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
