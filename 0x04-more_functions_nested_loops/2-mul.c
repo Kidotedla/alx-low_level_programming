dd61938b3ef67fc846fb17ad9c9b001a20d224d5
kidotedla <tedlakidus0@gmail.com>
2021-10-25 10:57:01 -0700

    0x04 mor fun

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply two numbers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
