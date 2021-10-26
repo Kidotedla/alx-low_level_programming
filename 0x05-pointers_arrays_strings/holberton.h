a6808b6a07dce2bf94f536ceef777fd8b55f0327
kidotedla <tedlakidus0@gmail.com>
2021-10-26 09:53:22 -0700

    0x05 pas

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+
+void reset_to_98(int *p);
+int _strlen(char *s);
+void rev_string(char *s);
+void print_rev(char *str);
+void _puts(char *str);
+void swap_int(int *a, int *b);
+int _putchar(char c);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
